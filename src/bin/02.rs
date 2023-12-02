4cc40c42c5ba0bba11313020c122ea5cc08c98a4
Isaiah Lemmon <lemmoi@cs.washington.edu>
2023-12-01 23:12:49 -0800

    day 2 without regex

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,6 @@
 advent_of_code::solution!(2);
 
-use regex::Regex;
 use std::cmp;
-
-
 
 pub fn part_one(input: &str) -> Option<u32> {
     run(input, |i, r, g, b| {
@@ -16,33 +13,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    run(input, |_, r, g, b| {Some(r * g * b)})
+    run(input, |_, r, g, b| Some(r * g * b))
 }
 
-fn run<F>(input: &str, f: F) -> Option<u32> where F: Fn(u32, u32, u32, u32) -> Option<u32> {
-    let re = Regex::new(r"(\d+) (b|r|g)").unwrap();
-    Some(input.lines().enumerate().map(|(game, line)| {
-            let rounds = line[5..].split(';');
-            let mut r: u32 = 0;
-            let mut b: u32 = 0;
-            let mut g: u32 = 0;
-            for round in rounds {
-                re.captures_iter(round).for_each(|entry| { 
-                    let (_, [count, letter]) = entry.extract();
-                    let num: u32 = count.parse().unwrap();
-                    match letter {
-                        "r" => r = cmp::max(r, num),
-                        "b" => b = cmp::max(b, num),
-                        "g" => g = cmp::max(g, num),
-                        _ => panic!("frick")
+fn run<F>(input: &str, f: F) -> Option<u32>
+where
+    F: Fn(u32, u32, u32, u32) -> Option<u32>,
+{
+    Some(
+        input
+            .lines()
+            .enumerate()
+            .map(|(game, line)| {
+                let (_, data) = line.split_once(':').unwrap();
+                let rounds = data.split(';');
+                let mut r: u32 = 0;
+                let mut b: u32 = 0;
+                let mut g: u32 = 0;
+                for round in rounds {
+                    for draw in round.split(',') {
+                        let (count, color) = draw.trim().split_once(' ').unwrap();
+                        let num: u32 = count.parse().unwrap();
+                        match color {
+                            "red" => r = cmp::max(r, num),
+                            "blue" => b = cmp::max(b, num),
+                            "green" => g = cmp::max(g, num),
+                            _ => panic!("frick"),
+                        }
                     }
-                });
-            }
-            f(game as u32, r, g, b)
-        }).filter_map(|x| x)
-        .sum())
+                }
+                f(game as u32, r, g, b)
+            })
+            .flatten()
+            .sum(),
+    )
 }
-
 
 #[cfg(test)]
 mod tests {
